d218deb0985c99886d087605d9e790248acba7e8
Jangseongjin <sungjin5891@gmail.com>
2021-05-08 14:44:34 +0900

    Refeactoring Calculator

--- calculator/src/com/refactoring/Calculator.java
+++ calculator/src/com/refactoring/Calculator.java
@@ -4,19 +4,24 @@ public class Calculator {
     public static int sum(String str) {
         int result = 0;
 
-        if(str.equals("") || str.equals(",")) {
-            return 0;
-        } else {
-            str = str.replaceAll(" ", "");
-            String[] numbers = str.split(",");
-            for(String num : numbers) {
-                if(!num.equals("")) {
-                    int numToInt = Integer.parseInt(num);
-                    result += numToInt;
-                }
+        if(isBlank(str)) return 0;
+
+        for(String num : stringToArray(str)) {
+            if(!num.equals("")) {
+                int numToInt = Integer.parseInt(num);
+                result += numToInt;
             }
         }
 
         return result;
+    }
+
+    private static Boolean isBlank(String str) {
+        return str.equals("") || str.equals(",");
+    }
+
+    private static String[] stringToArray(String str) {
+        str = str.replaceAll(" ", "");
+        return str.split(",");
     }
 }
