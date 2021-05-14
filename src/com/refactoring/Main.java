9af544375c129e423c1af41905b309b646bcf84f
장성진 <sungjin@payme.co.kr>
2021-05-14 09:22:26 +0900

    Lotto Project Step03

--- src/com/refactoring/Main.java
+++ src/com/refactoring/Main.java
@@ -1,11 +1,20 @@
 package com.refactoring;
 
 import com.refactoring.calculator.Calculator;
+import com.refactoring.lotto.Lotto;
 
 
 public class Main {
     public static void main(String[] args) {
-        int result = Calculator.sum("1,1,2,3,4,1,");
-        System.out.println(result);
+//        int result = Calculator.sum("1,2,3");
+//        System.out.println(result);
+
+        int[] arr = {35,19,13,9,23,44,38};
+
+        for (int i=0; i<1000; i++){
+            Lotto lotto = new Lotto(arr);
+            lotto.start();
+        }
+
     }
 }
