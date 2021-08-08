43de1d5b64116a155696d3b49f43a02c3a63aa9a
Jangseongjin <sungjin5891@gmail.com>
2021-08-08 18:38:20 +0900

    프로젝트 구조 변경 및 RPG 게임 추가

---
+++ RPGGame/src/main/java/com/example/rpggame/Main.java
@@ -0,0 +1,11 @@
+package com.example.rpggame;
+
+/**
+ * Created by marathoner on 2021/08/08
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
